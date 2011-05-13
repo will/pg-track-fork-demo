cda7791fa865803d219c147f1a839d43aa92956e
Will Leinweber <will@bitfission.com>
2011-05-13 13:40:57 -0700

    set color

--- web.rb
+++ web.rb
@@ -6,7 +6,7 @@ require 'sequel'
 class HPGDemo < Sinatra::Base
   set :public, File.dirname(__FILE__) + '/public'
 
-  PROPERTIES = %w[state state_updated_at state_checked_at status plan created_at current_transaction forked_from tracking]
+  PROPERTIES = %w[state state_updated_at state_checked_at status plan created_at current_transaction forked_from tracking elastic_ip]
   get '/' do
     haml :index
   end
@@ -26,11 +26,26 @@ class HPGDemo < Sinatra::Base
     JSON.dump db
   end
 
+  post '/dbs/:name' do |name|
+    url = ENV["HEROKU_POSTGRESQL_#{name.upcase}_URL"]
+    set_color(url, params['color'])
+  end
+
   def dbs
     ENV.keys.map do |k|
       k=~/^HEROKU_POSTGRESQL_(\w+)_URL$/
       $+
     end.compact
+  end
+
+  def set_color(url,color)
+    begin
+      db = Sequel.connect(url)
+      db["update list set val='#{color}' where key='color'"].first
+      db.disconnect
+    rescue => e
+      puts e.inspect
+    end
   end
 
   def get_color(url)
